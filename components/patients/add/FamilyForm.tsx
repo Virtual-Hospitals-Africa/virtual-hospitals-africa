20dc9a29ea4c1256a85c027b8b91fcf73e77e8e9
debruler <debrulerij@vcu.edu>
2023-09-28 13:37:16 -0400

    formatting fixes

--- components/patients/add/FamilyForm.tsx
+++ components/patients/add/FamilyForm.tsx
@@ -1,69 +1,65 @@
-import { SelectInput, TextInput } from "../../library/form/Inputs.tsx"
-import FormRow from "../../library/form/Row.tsx"
+import { SelectInput, TextInput } from '../../library/form/Inputs.tsx'
+import FormRow from '../../library/form/Row.tsx'
 import SectionHeader from '../../library/typography/SectionHeader.tsx'
 import Buttons from '../../library/form/buttons.tsx'
 import { AddPatientDataProps } from '../../../routes/app/patients/add.tsx'
-import ReligionSelect from "../../../islands/ReligionSelect.tsx"
+import ReligionSelect from '../../../islands/ReligionSelect.tsx'
 type FamilyFormProps = AddPatientDataProps['family']
 
 export default function FamilyForm(
-    { initialData = {} }: {initialData: Partial<FamilyFormProps> }, //might be unnecessary? ask!
+  { initialData = {} }: { initialData: Partial<FamilyFormProps> }, //might be unnecessary? ask!
 ) {
-    const allRelations = [
-        'Wife',
-        'Husband',
-        'Brother',
-        'Sister',
-        'Grandparent',
-        'Grandchild',
-        'Son',
-        'Daughter',
-        'Uncle',
-        'Aunt',
-        'Cousin',
-        'Other Relative or Friend',
-    ]
+  const allRelations = [
+    'Wife',
+    'Husband',
+    'Brother',
+    'Sister',
+    'Grandparent',
+    'Grandchild',
+    'Son',
+    'Daughter',
+    'Uncle',
+    'Aunt',
+    'Cousin',
+    'Other Relative or Friend',
+  ]
 
-    return (
-        <>
-            <FormRow>
-                <SelectInput name='marital status' required label='Marital Status'>
-                    <option value='Select'>Select</option>
-                    <option value='single'>Single</option>
-                    <option value='married'>Married</option>
-                    <option value='civilPartner'>Civil Partner</option>
-                    <option value='widowWidower'>Widow/Widower</option>
-                    <option value='separated'>Separated</option>
-                    <option value='divorced'>Divorced</option>
-                </SelectInput>
-                <ReligionSelect />
-            </FormRow>
-            <section>
-                
-                <SectionHeader className='my-5 text-[20px]'>Next of Kin</SectionHeader>
-                <FormRow>
-                    <TextInput name='Name' required label = 'Name'/>
-                    <TextInput name='phone number'/>
-                    <SelectInput name='relationship' required label = 'Relationship'>
-                        <option>Select</option>
-                        {allRelations.map((relation) => (
-                            <option>{relation}</option>
-                        ))}
-                    </SelectInput>
-                </FormRow>
-                </section>
-                <section>
-                    <SectionHeader className='my-5 text-[20px]'>Dependents</SectionHeader>
-                    {/* <AddDependants /> */}
-                     
-                </section>
-                <hr className='my-5' />
-            <section>
-                <Buttons
-                    submitText='Next Step'
-                    cancelHref='/app/patients'
-                />
-            </section>
-        </>
-    )
+  return (
+    <>
+      <FormRow>
+        <SelectInput name='marital status' required label='Marital Status'>
+          <option value='Select'>Select</option>
+          <option value='single'>Single</option>
+          <option value='married'>Married</option>
+          <option value='civilPartner'>Civil Partner</option>
+          <option value='widowWidower'>Widow/Widower</option>
+          <option value='separated'>Separated</option>
+          <option value='divorced'>Divorced</option>
+        </SelectInput>
+        <ReligionSelect />
+      </FormRow>
+      <section>
+        <SectionHeader className='my-5 text-[20px]'>Next of Kin</SectionHeader>
+        <FormRow>
+          <TextInput name='Name' required label='Name' />
+          <TextInput name='phone number' />
+          <SelectInput name='relationship' required label='Relationship'>
+            <option>Select</option>
+            {allRelations.map((relation) => <option>{relation}</option>)}
+          </SelectInput>
+        </FormRow>
+      </section>
+      <section>
+        <SectionHeader className='my-5 text-[20px]'>Dependents</SectionHeader>
+        {/* <AddDependants /> */}
+      </section>
+      <hr className='my-5' />
+      <section>
+        <Buttons
+          submitText='Next Step'
+          cancelHref='/app/patients'
+        />
+      </section>
+    </>
+  )
 }
--- islands/ReligionSelect.tsx
+++ islands/ReligionSelect.tsx
@@ -1,37 +1,44 @@
-import { SelectInput, TextInput } from "../components/library/form/Inputs.tsx"
-import { useState } from "preact/hooks"
+import { SelectInput, TextInput } from '../components/library/form/Inputs.tsx'
+import { useState } from 'preact/hooks'
 
-export default function ReligionSelect () {
-    const [selectedReligion, setSelectedReligion] = useState("none");
-    const allReligions = [
-        'Roman Catholic',
-        'Pentecostal/Protestant',
-        'Christianity',
-        'Islam',
-        'Buddhism',
-        'Apostolic Sect',
-        'African Traditional Religion',
-        'Non-Religious'
-    ]
+export default function ReligionSelect() {
+  const [selectedReligion, setSelectedReligion] = useState('none')
+  const allReligions = [
+    'Roman Catholic',
+    'Pentecostal/Protestant',
+    'Christianity',
+    'Islam',
+    'Buddhism',
+    'Apostolic Sect',
+    'African Traditional Religion',
+    'Non-Religious',
+  ]
 
-    const selectedOther = selectedReligion === 'other'
+  const selectedOther = selectedReligion === 'other'
 
-    return (
-        <div className='w-full'>
-            <SelectInput name={selectedOther? '' : 'religion'} required label='Religion' onChange={(e) => {
-                const selectedReligion = e?.currentTarget?.value;
-                setSelectedReligion(selectedReligion);
-                console.log(selectedReligion);
-            }}>
-                <option>Select</option>
-                {allReligions.map((religion) =>(
-                    <option value={religion} selected={selectedReligion === religion}>{religion}</option>
-                ))}
-                <option value='other'>Other</option>
-            </SelectInput>
-            {
-                selectedOther && <TextInput name='religion' label='' placeholder='Religion' />
-            }
-        </div>
-    )
+  return (
+    <div className='w-full'>
+      <SelectInput
+        name={selectedOther ? '' : 'religion'}
+        required
+        label='Religion'
+        onChange={(e) => {
+          const selectedReligion = e?.currentTarget?.value
+          setSelectedReligion(selectedReligion)
+          console.log(selectedReligion)
+        }}
+      >
+        <option>Select</option>
+        {allReligions.map((religion) => (
+          <option value={religion} selected={selectedReligion === religion}>
+            {religion}
+          </option>
+        ))}
+        <option value='other'>Other</option>
+      </SelectInput>
+      {selectedOther && (
+        <TextInput name='religion' label='' placeholder='Religion' />
+      )}
+    </div>
+  )
 }
