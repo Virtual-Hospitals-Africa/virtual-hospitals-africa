1da67fde819e3c73c296f7c7d62ef84f63e41eda
Rishab Sanyal <rsanyal@ucdavis.edu>
2023-10-26 16:47:51 -0700

    Rebase

--- test/models/health_workers.test.ts
+++ test/models/health_workers.test.ts
@@ -224,7 +224,7 @@ describe('db/models/health_workers.ts', { sanitizeResources: false }, () => {
           ncz_registration_card_media_id: null,
           face_picture_media_id: null,
           approved_by: null,
-          address: 'test address'
+          address: 'test address',
         },
       })
 
@@ -329,7 +329,7 @@ describe('db/models/health_workers.ts', { sanitizeResources: false }, () => {
           ncz_registration_card_media_id: registrationCardMedia.id,
           face_picture_media_id: facePictureMedia.id,
           approved_by: null,
-          address: 'test address'
+          address: 'test address',
         },
       })
 
