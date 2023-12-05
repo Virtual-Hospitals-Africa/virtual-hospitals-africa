eb7b72b04559694fe277abc8c07c7d2a27b147e1
ExplanationMark98 <johnduffer1@gmail.com>
2023-12-05 16:31:17 -0500

    Co-authored-by: Isabelle deBruler <debruler@users.noreply.github.com> Co-authored-by: aislinjohn <aislinjohn@users.noreply.github.com>

--- islands/ConditionSearch.tsx
+++ islands/ConditionSearch.tsx
@@ -6,6 +6,10 @@ import SearchResults, {
   ConditionSearchResult,
 } from '../components/library/SearchResults.tsx'
 import RemoveIcon from '../components/library/icons/remove.tsx'
+import MedicationSearch from '../islands/MedicationSearch.tsx'
+import PlusIcon from '../components/library/icons/plus.tsx'
+import AddIcon from '../components/library/icons/add.tsx'
+import { Condition } from '../types.ts'
 
 export default function ConditionSearch() {
   const [searchTerm, setSearchTerm] = useState('')
@@ -13,6 +17,9 @@ export default function ConditionSearch() {
   const searchInputRef = useRef<HTMLInputElement | null>(null)
   const [results, setResults] = useState<null | [string, string][]>(null)
   const [selectedConditions, setSelectedConditions] = useState<string[]>([])
+  //const [numConditions, setNumConditions] = useState<number>(0)
+  const [numConditions, setNumConditions] = useState<Condition[]>([])
+  const [addAnother, setAddAnother] = useState(0)
 
   const handleSearch = async (query: string) => {
     try {
@@ -26,6 +33,48 @@ export default function ConditionSearch() {
     } catch (error) {
       console.error('Error fetching data:', error)
     }
+  }
+
+  function IsabellesIdea() {
+    return (
+      <div>This is a test</div>
+      // <>
+      //   <FormRow>
+      //     <SearchInput
+      //       label=''
+      //       placeholder='Search for conditions'
+      //       value={searchTerm}
+      //       onInput={(e) => {
+      //         assert(e.target)
+      //         assert('value' in e.target)
+      //         assert(typeof e.target.value === 'string')
+      //         setSearchTerm(e.target.value)
+      //         setIsFocused(true)
+      //         handleSearch(e.target.value)
+      //       }}
+      //       onFocus={() => setIsFocused(true)}
+      //       ref={searchInputRef}
+      //     />
+      //   </FormRow>
+      //   <FormRow className='mb-3 relative'>
+      //     {isFocused && searchTerm && results?.length && (
+      //       <SearchResults>
+      //         {results.map((condition) => (
+      //           <ConditionSearchResult
+      //             condition={condition[0]}
+      //             onSelect={() => toggleConditionList(condition[0])}
+      //           />
+      //         ))}
+      //       </SearchResults>
+      //     )}
+      //   </FormRow>
+      // </>
+    )
+  }
+
+  const incrementAddAnother = () => {
+    setAddAnother((prevAddAnother) => prevAddAnother + 1)
+    IsabellesIdea()
   }
 
   const toggleConditionList = (condition: string) => {
@@ -58,24 +107,27 @@ export default function ConditionSearch() {
 
   return (
     <>
-      <FormRow>
-        <SearchInput
-          label=''
-          placeholder='Search for conditions'
-          value={searchTerm}
-          onInput={(e) => {
-            assert(e.target)
-            assert('value' in e.target)
-            assert(typeof e.target.value === 'string')
-            setSearchTerm(e.target.value)
-            setIsFocused(true)
-            handleSearch(e.target.value)
-          }}
-          onFocus={() => setIsFocused(true)}
-          ref={searchInputRef}
-        />
-      </FormRow>
+      <button onClick={incrementAddAnother}>plus</button>
 
+      {numConditions.map((condition) => (
+        <FormRow>
+          <SearchInput
+            label=''
+            placeholder='Search for conditions'
+            value={searchTerm}
+            onInput={(e) => {
+              assert(e.target)
+              assert('value' in e.target)
+              assert(typeof e.target.value === 'string')
+              setSearchTerm(e.target.value)
+              setIsFocused(true)
+              handleSearch(e.target.value)
+            }}
+            onFocus={() => setIsFocused(true)}
+            ref={searchInputRef}
+          />
+        </FormRow>
+      ))}
       <FormRow className='mb-3 relative'>
         {isFocused && searchTerm && results?.length && (
           <SearchResults>
