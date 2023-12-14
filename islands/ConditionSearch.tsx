cb1e74d2f5eb30d39de6380308ed5e1f9f72263e
debruler <debrulerij@vcu.edu>
2023-12-13 19:23:47 -0500

    wip Co-authored-by: ExplanationMark98 <ExplanationMark98@users.noreply.github.com> Co-authored-by: aislinjohn <aislinjohn@users.noreply.github.com>

--- islands/ConditionSearch.tsx
+++ islands/ConditionSearch.tsx
@@ -4,17 +4,34 @@ import RemoveIcon from '../components/library/icons/remove.tsx'
 import AddIcon from '../components/library/icons/add.tsx'
 
 
+
 export default function ConditionSearch() {
   const [isFocused, setIsFocused] = useState(false)
   const searchInputRef = useRef<HTMLInputElement | null>(null)
   const [results, setResults] = useState<null | [string, string][]>(null)
   const [isShown, setIsShown] = useState(false)
   const [numConditions, setNumConditions] = useState (0)
+  const [myArray, setMyArray] = useState<condition[]>([]);
+  
+  type condition  = {
+    id: number
+  }
 
   const showConditionSearchModule = () => {
     setIsShown(!isShown)
     setNumConditions(numConditions + 1)
   }
+
+  ////////////
+  //Trying Array Solution
+  const handleAddClick = () => {
+    const newId = myArray.length + 1;
+    setMyArray([...myArray, { id: newId }]);
+  };
+  const handleRemoveClick = (id: number) => {
+    setMyArray(myArray.filter(item => item.id !== id));
+  };
+  //////////
 
   useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
@@ -34,11 +51,27 @@ export default function ConditionSearch() {
 
   return (
     <>
-      <button onClick={showConditionSearchModule} type="button">add condition</button>
-      {/* {isShown && <ConditionSearchModule />} */}
+      {/* <button onClick={showConditionSearchModule} type="button"><AddIcon></AddIcon></button>
       {Array.from({ length: numConditions }, (_, index) => (
-        <ConditionSearchModule />
-      ))}
+          <div key={index} className='flex flex-row'>
+            <button type="button"><RemoveIcon></RemoveIcon></button>
+            <ConditionSearchModule />
+          </div>
+      
+      ))} */}
+            
+
+        {myArray.map((item) => (
+          <div key = {item.id} className='flex flex-row'>
+              <button onClick={() => handleRemoveClick(item.id)} className='mr-2'><RemoveIcon></RemoveIcon></button>
+              <ConditionSearchModule/>
+          </div>))}
+          <button onClick={handleAddClick} type="button" className='flex flex-row text-[#4F46E5] font-bold'><AddIcon></AddIcon>Add Condition</button>
+
+            
+
+
+
 
       
       
