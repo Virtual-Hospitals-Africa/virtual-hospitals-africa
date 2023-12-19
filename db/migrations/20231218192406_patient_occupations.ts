f441962c423a468d39a7866facb9179f8b6f21ec
ExplanationMark98 <johnduffer1@gmail.com>
2023-12-19 13:28:23 -0500

    Commiting to Branch

---
+++ db/migrations/20231218192406_patient_occupations.ts
@@ -0,0 +1,21 @@
+import { Kysely } from "kysely";
+import { addUpdatedAtTrigger } from '../addUpdatedAtTrigger.ts';
+
+
+export async function up(db: Kysely<unknown>) {
+    await db.schema
+    .createTable('patient_occupations')
+    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('patient_id', 'integer', (col) =>
+    col
+      .notNull()
+      .references('patients.id'))
+    .addColumn('school', 'json') //All Information Collect
+    .addColumn('job', 'json')
+    .execute()
+    await addUpdatedAtTrigger(db, 'patient_occupations')
+}
+
+export async function down(db: Kysely<unknown>) {
+    await db.schema.dropTable('patient_guardians').execute()
+}
