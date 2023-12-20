7f4ac0ec717fd474f153d78f9c0de8f4e15008cc
debruler <debrulerij@vcu.edu>
2023-12-20 12:36:20 -0500

    fix

--- components/patients/add/OccupationForm.tsx
+++ components/patients/add/OccupationForm.tsx
@@ -1,9 +1,6 @@
-import AllergySearch from '../../../islands/AllergySearch.tsx'
-import ConditionSearch from '../../../islands/ConditionSearch.tsx'
 import { OnboardingPatient } from '../../../types.ts'
 import Occupation0_18 from '../../../islands/Occupation0-18.tsx'
-import FormRow from '../../library/form/Row.tsx'
-import { TextInput } from '../../library/form/Inputs.tsx'
+
 export default function PatientOccupationForm(
   { patient = {} }: { patient?: Partial<OnboardingPatient> },
 ) {
