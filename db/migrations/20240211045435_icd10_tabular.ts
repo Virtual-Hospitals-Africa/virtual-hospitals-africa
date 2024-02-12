19004220b039859240bb05f35a6d3f9052d6a418
Will Weiss <will.weiss1230@gmail.com>
2024-02-12 10:15:57 -0500

    reorder checkboxes

--- db/migrations/20240211045435_icd10_tabular.ts
+++ db/migrations/20240211045435_icd10_tabular.ts
@@ -2,12 +2,14 @@
 import { Kysely, sql } from 'kysely'
 import { XMLParser } from 'fast-xml-parser'
 import isObjectLike from '../../util/isObjectLike.ts'
+import { assert } from 'std/assert/assert.ts'
+import compact from '../../util/compact.ts'
 
-function collapseNotes(term: unknown): string | null {
+function collapseNotes(term: unknown): string[] | string | null {
   if (!term) return null
   if (typeof term === 'string') return term
   if (Array.isArray(term)) {
-    return term.map(collapseNotes).join('\n')
+    return compact(term.flatMap(collapseNotes))
   }
   if (isObjectLike(term) && 'note' in term) {
     return collapseNotes(term.note)
@@ -17,16 +19,41 @@ function collapseNotes(term: unknown): string | null {
 
 export async function up(db: Kysely<any>) {
   await db.schema.createTable('icd10_tabular')
-    .addColumn('code', 'varchar(7)', (col) => col.primaryKey())
+    .addColumn('code', 'varchar(8)', (col) => col.primaryKey())
+    .addColumn('category', 'varchar(3)', (col) => col.notNull())
     .addColumn('description', 'varchar(255)', (col) => col.notNull())
-    .addColumn('excludes1', 'text')
-    .addColumn('excludes2', 'text')
     .addColumn('includes', 'text')
     .addColumn(
       'parent_code',
-      'varchar(6)',
+      'varchar(7)',
       (col) => col.references('icd10_tabular.code'),
     )
+    .execute()
+
+  await db.schema.createTable('icd10_tabular_exclude1_note')
+    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('code', 'varchar(8)', (col) => col.references('icd10_tabular.code'))
+    .addColumn('note', 'varchar(255)', (col) => col.notNull())
+    .execute()
+
+  await db.schema.createTable('icd10_tabular_exclude1_excludes')
+    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('exclude1_note_id', 'integer', (col) => col.references('icd10_tabular_exclude1_note.id'))
+    .addColumn('exclude_code', 'varchar(8)', (col) => col.references('icd10_tabular.code'))
+    .addColumn('dash', 'boolean', (col) => col.notNull())
+    .execute()
+
+  await db.schema.createTable('icd10_tabular_exclude2_note')
+    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('code', 'varchar(8)', (col) => col.references('icd10_tabular.code'))
+    .addColumn('note', 'varchar(255)', (col) => col.notNull())
+    .execute()
+
+  await db.schema.createTable('icd10_tabular_exclude2_excludes')
+    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('exclude2_note_id', 'integer', (col) => col.references('icd10_tabular_exclude2_note.id'))
+    .addColumn('exclude_code', 'varchar(8)', (col) => col.references('icd10_tabular.code'))
+    .addColumn('dash', 'boolean', (col) => col.notNull())
     .execute()
 
   await sql`
@@ -39,6 +66,8 @@ export async function up(db: Kysely<any>) {
   ).then(
     (data) => new XMLParser().parse(data)['ICD10CM.tabular']['chapter'],
   )
+
+  const sections = chapters.flatMap((chapter: any) => Array.isArray(chapter.section) ? chapter.section : [chapter.section])
 
   async function iterTree(tree: any, parent_code: string | null = null) {
     if (Array.isArray(tree)) {
@@ -56,13 +85,19 @@ export async function up(db: Kysely<any>) {
       inclusionTerm,
     } = tree
     if (code) {
+      const [category] = code.split('.')
+      assert(category.length === 3)
+      const includes_col = collapseNotes(includes) || collapseNotes(inclusionTerm)
+      assert(includes_col === null || typeof includes_col === 'string' || (Array.isArray(includes_col) && includes_col.length === 1))
+
       await db.insertInto('icd10_tabular')
         .values({
           code,
+          category,
           description,
           excludes1: collapseNotes(excludes1),
           excludes2: collapseNotes(excludes2),
-          includes: collapseNotes(includes) || collapseNotes(inclusionTerm),
+          includes: Array.isArray(includes_col) ? includes_col[0] : includes_col,
           parent_code,
         })
         .execute()
@@ -72,7 +107,7 @@ export async function up(db: Kysely<any>) {
     }
   }
 
-  await iterTree(chapters)
+  await iterTree(sections)
 }
 
 export function down(db: Kysely<unknown>) {
