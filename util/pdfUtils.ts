fcb77c2bbc656ff8e14cb08b09f01b1ce0d79775
SeventyThree73 <2294387616@qq.com>
2024-07-01 22:04:40 +0100

    Updated the command execution method

--- util/pdfUtils.ts
+++ util/pdfUtils.ts
@@ -1,26 +1,23 @@
-import { exec } from 'node:child_process';
-import util from 'node:util';
-import fs from 'node:fs';
-import crypto from 'node:crypto';
+import fs from 'node:fs'
+import crypto from 'node:crypto'
+import { runCommand } from '../util/command.ts'
 
 const prefix = 'https://localhost:8000';
-const execPromise = util.promisify(exec);
 
 export async function generatePDF(url: string): Promise<string> {
   const filename = crypto.createHash('md5').update(url).digest('hex');
   const outputPath = `../${filename}.pdf`; // Should I create a temporary space to store the file? If so, what should I name it?
   const fullUrl = url.startsWith(prefix) ? url : `${prefix}${url}`;
-  const command = `wkhtmltopdf ${fullUrl} ${outputPath}`;
 
   try {
-    await execPromise(command);
-    // console.log(`PDF generated: ${outputPath}`);
-    return outputPath;
+    await runCommand('wkhtmltopdf', {
+        args: [fullUrl, outputPath],
+      })
   } catch (error) {
     console.error(`Error generating PDF: ${error}`);
-    return outputPath;
-    // throw error;
   }
+
+  return outputPath;
 }
 
 export function deletePDF(filePath: string): void {
