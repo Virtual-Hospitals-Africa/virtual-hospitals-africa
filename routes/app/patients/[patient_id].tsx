a6fbcd76480a4404bb015e5a5bb818568d5b2b3b
Saad Malik Paracha <35294802+saadmalik96@users.noreply.github.com>
2024-11-06 13:02:44 -0800

    Removed old content, added basic patient info to profile page

--- routes/app/patients/[patient_id].tsx
+++ routes/app/patients/[patient_id].tsx
@@ -1,23 +1,19 @@
 import * as patients from '../../../db/models/patients.ts'
-import PatientDetailedCard from '../../../components/patients/DetailedCard.tsx'
 import Layout from '../../../components/library/Layout.tsx'
-import SectionHeader from '../../../components/library/typography/SectionHeader.tsx'
-import { Button } from '../../../components/library/Button.tsx'
 import { assertOr404 } from '../../../util/assertOr.ts'
 import { LoggedInHealthWorkerContext } from '../../../types.ts'
 import { getRequiredUUIDParam } from '../../../util/getParam.ts'
 import redirect from '../../../util/redirect.ts'
+import PatientBasicInfo from '../../../components/patients/PatientBasicInfo.tsx'
 
 export default async function PatientPage(
   _req: Request,
   ctx: LoggedInHealthWorkerContext,
 ) {
-  const { healthWorker } = ctx.state
   const patient_id = getRequiredUUIDParam(ctx, 'patient_id')
 
-  const [patient] = await patients.getWithOpenEncounter(ctx.state.trx, {
-    ids: [patient_id],
-    health_worker_id: healthWorker.id,
+  const patient = await patients.getByID(ctx.state.trx, {
+    id: patient_id,
   })
 
   assertOr404(patient, 'Patient not found')
@@ -34,17 +30,8 @@ export default async function PatientPage(
       health_worker={ctx.state.healthWorker}
       variant='practitioner home page'
     >
-      <div className='mt-4 divide-y divide-gray-100 text-sm leading-6 lg:col-span-7 xl:col-span-8 row-span-full'>
-        {patient.open_encounter && (
-          <Button href={`${ctx.url.pathname}/encounters/open/vitals`}>
-            Go to open encounter
-          </Button>
-        )}
-
-        <SectionHeader>
-          Demographic Data
-        </SectionHeader>
-        <PatientDetailedCard patient={patient} />
+      <div className="container my-4 mx-6">
+        <PatientBasicInfo patient={patient} />
       </div>
     </Layout>
   )
