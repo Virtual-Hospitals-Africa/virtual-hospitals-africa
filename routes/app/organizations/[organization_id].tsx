4478f773dbc3f3266b3be69689df4b44cbf5f56f
laurencelo1 <llo7@dons.usfca.edu>
2024-11-18 03:20:34 -0800

    populate organization page

--- routes/app/organizations/[organization_id].tsx
+++ routes/app/organizations/[organization_id].tsx
@@ -16,7 +16,42 @@ export default async function OrganizationPage(
       health_worker={ctx.state.healthWorker}
       variant='practitioner home page'
     >
-      TODO: organization page
+      `<div>
+        <div class='px-4 sm:px-0'>
+          <h3 class='text-base/7 font-semibold text-gray-900'>
+            {organization.name}
+          </h3>
+          <p class='mt-1 max-w-2xl text-sm/6 text-gray-500'>
+            {organization.address}
+          </p>
+        </div>
+        <div class='mt-6 border-t border-gray-100'>
+          <dl class='divide-y divide-gray-100'>
+            <div class='bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3'>
+              <dt class='text-sm/6 font-medium text-gray-900'>
+                Organization Type
+              </dt>
+              <dd class='mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0'>
+                {organization.category}
+              </dd>
+            </div>
+            <div class='bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3'>
+              <dt class='text-sm/6 font-medium text-gray-900'>Address</dt>
+              <dd class='mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0'>
+                {organization.address}
+              </dd>
+            </div>
+            <div class='bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3'>
+              <dt class='text-sm/6 font-medium text-gray-900'>
+                Organization ID
+              </dt>
+              <dd class='mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0'>
+                {organization.id}
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>`
     </Layout>
   )
 }
