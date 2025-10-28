c03d909c8565811d66a100d2fc899bdbee9cc0d9
Erik Withak <erikaanestad@gmail.com>
2025-10-28 13:05:52 -0700

    adds routing patient content

--- routes/app/organizations/[organization_id]/patients/[patient_id]/open_encounter/triage/route_patient.tsx
+++ routes/app/organizations/[organization_id]/patients/[patient_id]/open_encounter/triage/route_patient.tsx
@@ -1,20 +1,68 @@
+import { z } from 'zod'
+import { VerticalRadioButtons } from '../../../../../../../../components/library/VerticalRadioButtons.tsx'
+import { updateForOpenEncounterAfterCompletingWorkflow } from '../../../../../../../../db/models/patient_presence.ts'
+import { pluralize } from '../../../../../../../../util/pluralize.ts'
+import { postHandler } from '../../../../../../../../util/postHandler.ts'
+import { promiseProps } from '../../../../../../../../util/promiseProps.ts'
+import redirect from '../../../../../../../../util/redirect.ts'
 import {
   completeLastStep,
   nextRouteAfterCompletingWorkflow,
   OpenEncounterWorkflowContext,
   OpenEncounterWorkflowPage,
 } from '../_middleware.tsx'
-import { z } from 'zod'
-import { postHandler } from '../../../../../../../../util/postHandler.ts'
-import redirect from '../../../../../../../../util/redirect.ts'
-import { promiseProps } from '../../../../../../../../util/promiseProps.ts'
-import { updateForOpenEncounterAfterCompletingWorkflow } from '../../../../../../../../db/models/patient_presence.ts'
 
-const TriageRoutePatientSchema = z.object({})
+const TriageRoutePatientSchema = z.object({
+  triage_route_patient: z.enum([
+    'consultation',
+    'stabilization',
+    'call_for_help',
+  ]),
+})
+
+const senoirHealthCareWorker = 'Ramasela Le Roux'
+const patientName = 'Lukman'
+const targetTreatmentTime = 10
+
+const mockData = [
+  {
+    id: 'consultation',
+    name: 'Consultation with primary care department',
+    description: [
+      `I will transfer ${patientName} to the waiting room`,
+      `The next available health worker will see ${patientName}`,
+      `Target time to treatment: ${targetTreatmentTime} minute ${
+        pluralize(
+          'minute',
+          targetTreatmentTime,
+        )
+      }`,
+    ],
+  },
+  {
+    id: 'stablization',
+    name: 'Stabilization with emergency department',
+    description: [
+      `I will transfer ${patientName} to the stabilization area`,
+      `${senoirHealthCareWorker} will be notified immediately to meet us in the stabilization area`,
+    ],
+  },
+  {
+    id: 'call_for_help',
+    name: 'Call for help in triage area',
+    description: [
+      `I will stay here in the triage area with ${patientName}`,
+      `${senoirHealthCareWorker} will be notified immediately to join us in the triage area`,
+    ],
+  },
+]
+
+// https://localhost:8000/app/organizations/00000000-0000-0000-0000-000000000001/patients/fb486201-0fc8-49a6-8fb6-1e509fe69599/open_encounter/triage/route_patient
 
 export const handler = postHandler(
   TriageRoutePatientSchema,
   async (_req, ctx: OpenEncounterWorkflowContext, _form_values) => {
+    console.log('\n\nn===============', _form_values)
     const { trx, encounter, organization_employment } = ctx.state
 
     const { next_patient_presence } = await promiseProps({
@@ -36,7 +84,13 @@ export const handler = postHandler(
 export async function TriageRoutePatientPage(
   _ctx: OpenEncounterWorkflowContext,
 ) {
-  return <>TODO</>
+  return (
+    <VerticalRadioButtons
+      options={mockData}
+      name='triage_route_patient'
+      defaultValue='consultation'
+    />
+  )
 }
 
 export default OpenEncounterWorkflowPage(TriageRoutePatientPage)
