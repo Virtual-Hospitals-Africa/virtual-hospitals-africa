3d2b660a42dbbec28f7bf4cf86868b4fe413f12d
k-brandon98 <kbrandon98@proton.me>
2025-12-30 15:14:22 -0500

    merged from save emergency contacts

--- db.d.ts
+++ db.d.ts
@@ -19,10 +19,13 @@ export type DoctorReviewStep =
   | 'revert'
 
 export type EmergencyContactRelationship =
+  | 'Child'
   | 'Friend'
   | 'Other'
   | 'Parent'
+  | 'Partner'
   | 'Sibling'
+  | 'Spouse'
 
 export type EncounterReason =
   | 'administration'
