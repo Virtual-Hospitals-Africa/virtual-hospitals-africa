2eb1d35e95d0165945590b9bc19963a7bda08d70
k-brandon98 <kbrandon98@proton.me>
2026-01-06 16:01:17 -0500

    added functionality for data to stick when you reload the page

--- routes/app/organizations/[organization_id]/patients/[patient_id]/open_encounter/registration/contacts.tsx
+++ routes/app/organizations/[organization_id]/patients/[patient_id]/open_encounter/registration/contacts.tsx
@@ -11,7 +11,8 @@ import PatientContactInformationSection from '../../../../../../../../islands/Pa
 import EmergencyContactSection from '../../../../../../../../islands/EmergencyContactsSection.tsx'
 import { EmergencyContactSchema } from '../../../../../../../../shared/family.ts'
 
-const PatientRegistrationContactsSchema = z.object({
+
+export const PatientRegistrationContactsSchema = z.object({
   // address: z.object({
   //   street: z.string().optional(),
   //   locality: z.string(),
@@ -24,7 +25,6 @@ const PatientRegistrationContactsSchema = z.object({
 
 export const handler = postHandler(
   PatientRegistrationContactsSchema,
-  // deno-lint-ignore require-await
   async (
     ctx: OpenEncounterWorkflowContext,
     { emergency_contacts },
@@ -33,7 +33,7 @@ export const handler = postHandler(
       ctx.state.trx,
       { patient_id: ctx.state.patient.id, contacts: emergency_contacts },
     )
-    console.log('TODO use emergency_contacts', emergency_contacts)
+    // console.log('TODO use address', address)
     // await patient_address.updateById(
     //   ctx.state.trx,
     //   { patient_id: ctx.state.patient.id, address },
@@ -46,15 +46,18 @@ export const handler = postHandler(
 export async function PatientRegistrationContactsPage(
   ctx: OpenEncounterWorkflowContext,
 ) {
-  const address = await patient_address.getById(ctx.state.trx, {
+  const existing_address = await patient_address.getById(ctx.state.trx, {
     patient_id: ctx.state.patient.id,
   })
+  const existing_contacts = await patient_contacts.getByPatientId(
+    ctx.state.trx,
+    { patient_id: ctx.state.patient.id },
+  )
   return (
-    <>
-      {/* <AddressSection address={address} /> */}
-      <PatientContactInformationSection address={address} />
-      <EmergencyContactSection />
-    </>
+    <div>
+      <PatientContactInformationSection address={existing_address} />
+      <EmergencyContactSection existing_contacts={existing_contacts} />
+    </div>
   )
 }
 
