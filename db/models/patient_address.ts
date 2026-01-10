65d999290862c2d4868201c0b3ea96789d9b7e34
k-brandon98 <kbrandon98@proton.me>
2026-01-09 21:28:42 -0500

    Started google maps functionality

--- db/models/patient_address.ts
+++ db/models/patient_address.ts
@@ -1,5 +1,5 @@
 import * as addresses from './addresses.ts'
-import { TrxOrDb } from '../../types.ts'
+import { Address, TrxOrDb } from '../../types.ts'
 
 export function getById(trx: TrxOrDb, { patient_id }: { patient_id: string }) {
   return trx
@@ -14,16 +14,32 @@ export function getById(trx: TrxOrDb, { patient_id }: { patient_id: string }) {
     .executeTakeFirst()
 }
 
+function addressToInsert(address: Address): addresses.AddressInsert {
+  return {
+    street_number: address.street_number ?? undefined,
+    route: address.route ?? undefined,
+    unit: address.unit ?? undefined,
+    street: address.street ?? undefined,
+    locality: address.locality ?? '',
+    administrative_area_level_1: address.administrative_area_level_1 ??
+      undefined,
+    administrative_area_level_2: address.administrative_area_level_2 ??
+      undefined,
+    country: address.country,
+    postal_code: address.postal_code ?? undefined,
+  }
+}
+
 export async function updateById(
   trx: TrxOrDb,
   { patient_id, address }: {
     patient_id: string
-    address: addresses.AddressInsert
+    address: Address
   },
 ) {
   const created_address = await addresses.insert(
     trx,
-    address,
+    addressToInsert(address),
   )
   await trx.updateTable('patients')
     .where('patients.id', '=', patient_id)
