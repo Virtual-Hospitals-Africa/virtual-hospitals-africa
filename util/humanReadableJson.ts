551fd1875b1cd5493f83f335e7569aee54bad834
k-brandon98 <kbrandon98@proton.me>
2026-01-13 11:56:52 -0500

    rebased humanreadablejson

--- util/humanReadableJson.ts
+++ util/humanReadableJson.ts
@@ -3,6 +3,6 @@ import { MostlyJsonSerializable } from '../types.ts'
 
 const formatter = new Formatter()
 
-export function humanReadableJson(object: MostlyJsonSerializable): string {
+export function humanReadableJson(object: JsonSerializable): string {
   return formatter.Serialize(object)!
 }
