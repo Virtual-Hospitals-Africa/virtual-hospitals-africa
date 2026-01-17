d5a4cac75c2ba6f3aa6f8d0ad424232d5c9ba9ca
Will Weiss <will.weiss1230@gmail.com>
2026-01-16 21:11:42 -0500

    ok

--- test/web/patients/open_encounter/registration/route_patient.test.ts
+++ test/web/patients/open_encounter/registration/route_patient.test.ts
@@ -69,7 +69,7 @@ describeParallel(
             'Status': 'Awaiting Triage',
             'Employees': 'Next Available',
             'Arrived': z.enum(['Just now', '1 minute ago']),
-            'Actions': 'triage',
+            'Actions': 'Triage',
           },
         ], { strict: true })
       },
