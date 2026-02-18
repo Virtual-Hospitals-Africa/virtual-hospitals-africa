8e3afd537f76900e2182526b312a0387b083d612
Sikhululiwe Ngwenya <sikhululiwe.ngwenya@virtualhospitalsafrica.org>
2026-02-18 22:59:00 +0200

    Update ntasks.lisp
    Symptoms and conditions warranting urgent eye care and or referral

--- s_expression/ntasks.lisp
+++ s_expression/ntasks.lisp
@@ -255,12 +255,37 @@
   (check_for
     (clinical_finding (snomed_concept "Sees haloes around lights" "finding"))
     (clinical_finding (snomed_concept "Blurring of visual image" "finding"))
+		(clinical_finding ( snomed_concept “Weakness of face muscles” (finding) New (qualifier value) Sudden (qualifier value)Asymmetry (qualifier value)
+		(clinical_finding ( snomed_concept “Muscle weakness of upper limb” (finding) New (qualifier value) Sudden (qualifier value)Asymmetry (qualifier value)
+		(clinical_finding ( snomed_concept “Weakness of muscle of lower limb” (finding) New (qualifier value) Sudden (qualifier value)Asymmetry (qualifier value)
+		(clinical_finding ( snomed_concept “Numbness of face” (finding)
+		(clinical_finding ( snomed_concept “Numbness of limbs” (finding)
     (clinical_finding (snomed_concept "Sudden visual loss" "disorder"))
     (clinical_finding (snomed_concept "Corneal ulcer" "disorder"))
     (clinical_finding (snomed_concept "Corneal opacity" "disorder"))
     (clinical_finding (snomed_concept "Jaundice" "finding"))
-    (clinical_finding (snomed_concept "Herpes zoster keratoconjunctivitis" "disorder"))
-  )
+ 		(clinical_finding (snomed_concept “Not pregnant” (finding)
+		AND 
+		(>=measurement (snomed_concept “Systolic blood pressure” (observable entity) Millimeter of mercury (qualifier value)180))
+		(>=measurement ( (snomed_concept “Diastolic blood pressure “(observable entity) Millimeter of mercury (qualifier value)110))
+		(clinical_finding (snomed_concept “Pregnancy” (finding) OR <= Postpartum period, 7 days (finding)
+		AND 
+		(>=measurement ( (snomed_concept “Systolic blood pressure (observable 		entity)Millimeter of mercury (qualifier value)140))
+		(>=measurement ( (snomed_concept “Diastolic blood pressure (observable 	entity) Millimeter of mercury (qualifier value)90))
+(clinical_finding (snomed_concept “Swelling of eyelid” (finding) Both eyes, entire (body structure) Entire (qualifier value)
+(clinical_finding (snomed_concept “Red eye” (finding)
+(clinical_finding (snomed_concept “Pain in eye” (finding))
+(clinical_finding (snomed_concept “Abnormal vision” (finding) Sudden (qualifier value)
+(clinical_finding (snomed_concept “Herpes zoster involving tip of nose” (disorder)
+(clinical_finding (snomed_concept “Herpes zoster ophthalmicus” (disorder)
+(clinical_finding (snomed_concept “Penetrating wound of eye” (disorder)
+(clinical_finding (snomed_concept “Laceration of eyelid” (disorder)
+(clinical_finding (snomed_concept “Metal foreign body in eye region” (disorder)
+(clinical_finding (snomed_concept “Penetration of eyeball with magnetic foreign body” (disorder)
+(clinical_finding (snomed_concept “Chemical burn of head” (disorder)
+(clinical_finding (snomed_concept “Ptosis of eyelid” (disorder) Sudden (qualifier value)
+
+
 )
 ;; Page 32 - Face Symptoms
 (task
