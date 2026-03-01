c86f95c4cb5d9f910014cc3c13d4d8f10d77fe59
Sikhululiwe Ngwenya <sikhululiwe.ngwenya@virtualhospitalsafrica.org>
2026-03-01 16:57:07 +0200

    Update ntasks.lisp

--- s_expression/ntasks.lisp
+++ s_expression/ntasks.lisp
@@ -432,9 +432,17 @@
   (clinical_finding (snomed_concept "Neck pain" "finding"))
   (check_for
     (clinical_finding (snomed_concept "Stiff neck" "finding"))
+		(clinical_finding ( snomed_concept “Drowsy” (finding))
+		(clinical_finding ( snomed_concept “Clouded consciousness” (finding))
+		(clinical_finding ( snomed_concept “Purpuric rash” (disorder)
     (clinical_finding (snomed_concept "Numbness of limbs" "finding"))
     (clinical_finding (snomed_concept "Muscle weakness of limb" "finding"))
-    (clinical_finding (snomed_concept "Traumatic injury" "disorder"))
+		(clinical_finding ( snomed_concept “Clumsiness” (finding))
+		(clinical_finding ( snomed_concept “Stiffness” (finding))
+		(clinical_finding ( snomed_concept “Abnormal gait” (finding))
+		(clinical_finding ( snomed_concept “Decreased coordination” (finding))
+		(clinical_finding ( snomed_concept “Injury of neck” (disorder) Recent (qualifier value) Plain X-ray of cervical spine abnormal (finding))
+		(clinical_finding ( snomed_concept “Injury of neck” (disorder)Recent (qualifier value) Plain X-ray of neck (procedure) Not available (qualifier value))
   )
 )
 ;; Page 64 - Arm or Hand Symptoms
