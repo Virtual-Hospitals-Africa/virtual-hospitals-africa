c5ce58cd28936565156760a99a1c5f2ae56c3c61
Sikhululiwe Ngwenya <sikhululiwe.ngwenya@virtualhospitalsafrica.org>
2026-03-01 17:53:34 +0200

    Update ntasks.lisp

--- s_expression/ntasks.lisp
+++ s_expression/ntasks.lisp
@@ -482,17 +482,44 @@
   )
 )
 ;; Page 67 - Skin Symptoms
+;;Page 68 – Painful skin
+;;Page 69 – Generalized itch rash
+;;Page 70 – Localized itchy rash
+;;Page 71 – Itch with no rash
+;;Page 72 – Generalized non-itchy rash
 (task
   "Check for urgent skin symptom conditions"
   adult
-  (clinical_finding (snomed_concept "Eruption" "morphologic abnormality"))
+	(clinical_finding ( snomed_concept “Skin finding” (finding))
+	(clinical_finding ( snomed_concept “Skin-ache syndrome” (finding))
+	(clinical_finding ( snomed_concept “Generalized rash” (disorder) Itching 	(finding) “With” (attribute))
+	(clinical_finding ( snomed_concept “Localized eruption of skin” (disorder) “Itching” (finding) “With” (attribute))
+	(clinical_finding ( snomed_concept “Generalized rash” (disorder) “No itch” (situation) “With” (attribute))
   (check_for
+		(clinical_finding ( snomed_concept “Generalized pruritus” (finding))
+		(clinical_finding ( snomed_concept “Generalized rash” (disorder))
+		(clinical_finding ( snomed_concept “ Facial swelling” (finding))
+		(clinical_finding ( snomed_concept “Tongue swelling” (finding))
+		(clinical_finding ( snomed_concept “Wheezing” (finding))
+		(clinical_finding (snomed_concept “Difficulty breathing” (finding))
+		(clinical_finding (snomed_concept “Dizziness” (finding)) 							            		(clinical_finding ( snomed_concept “Collapse” (finding))
+		(clinical_finding ( snomed_concept “Exposure to” (contextual qualifier) “Substance” (substance) (qualifier value) “Possible” (qualifier value) “Allergen” (attribute))
     (clinical_finding (snomed_concept "Purpuric rash" "disorder"))
     (clinical_finding (snomed_concept "Stiff neck" "finding"))
-    (clinical_finding (snomed_concept "Eruption caused by drug" "disorder"))
+		(clinical_finding ( snomed_concept “Drowsy” (finding))
+		(clinical_finding ( snomed_concept “Clouded consciousness” (finding))
+		(clinical_finding ( snomed_concept “Headache” (finding))
+		(clinical_finding ( snomed_concept “Generalized rash” (disorder) “New medication commenced” (finding) “In the past” (attribute) “month” (qualifier value)3))
     (clinical_finding (snomed_concept "Jaundice" "finding"))
-    (clinical_finding (snomed_concept "Blister of skin" "disorder"))
+    (clinical_finding (snomed_concept "Blister” (morphologic abnormality))
     (clinical_finding (snomed_concept "Abdominal pain" "finding"))
+		(clinical_finding ( snomed_concept “Vomiting” (disorder))
+		(clinical_finding ( snomed_concept “Diarrhea” (finding))
+		(clinical_finding ( snomed_concept “Perioral dermatitis” (disorder))
+		(clinical_finding ( snomed_concept “Periocular dermatitis” (disorder))
+		(clinical_finding ( snomed_concept “Rash of genitalia” (disorder))
+		(clinical_finding ( snomed_concept “Peeling of skin” (finding))
+		(clinical_finding ( snomed_concept “Denuded skin” (disorder))
   )
 )
 ;; Page 79 - Jaundice
