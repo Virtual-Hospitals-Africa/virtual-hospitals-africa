82d11c45973d02049e60b97182166ef42953b856
Sikhululiwe Ngwenya <sikhululiwe.ngwenya@virtualhospitalsafrica.org>
2026-03-01 18:48:09 +0200

    Update ntasks.lisp

--- s_expression/ntasks.lisp
+++ s_expression/ntasks.lisp
@@ -493,6 +493,27 @@
     (clinical_finding (snomed_concept "Jaundice" "finding"))
     (clinical_finding (snomed_concept "Blister of skin" "disorder"))
     (clinical_finding (snomed_concept "Abdominal pain" "finding"))
+		)
+)
+;; Page 73 – Drug rash
+(task
+	“Check for urgent rash conditions with drug exposure”
+	adult
+	(clinical_finding ( snomed_concept “Eruption caused by drug” (disorder))
+	(check_for 
+		(clinical_finding ( snomed_concept “ Facial swelling” (finding))
+		(clinical_finding ( snomed_concept “Tongue swelling” (finding))
+		(clinical_finding (snomed_concept “Difficulty breathing” (finding))
+		(clinical_finding ( snomed_concept “Abdominal pain” (finding))
+		(clinical_finding ( snomed_concept “Vomiting” (disorder))
+		(clinical_finding ( snomed_concept “Diarrhea” (finding))
+		(clinical_finding ( snomed_concept “Perioral dermatitis” (disorder))
+		(clinical_finding ( snomed_concept “Periocular dermatitis” (disorder)
+		(clinical_finding ( snomed_concept “Rash of genitalia” (disorder))
+		(clinical_finding ( snomed_concept “Blister” (morphologic abnormality))
+		(clinical_finding ( snomed_concept “Peeling of skin” (finding))
+		(clinical_finding ( snomed_concept “Denuded skin” (disorder))
+		(clinical_finding ( snomed_concept “Jaundice” (finding))
   )
 )
 ;; Page 79 - Jaundice
