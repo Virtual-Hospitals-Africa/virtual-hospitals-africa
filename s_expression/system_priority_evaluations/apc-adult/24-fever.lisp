48c74db0cd613473ee2b13134662bcd4329e660d
Sikhululiwe Ngwenya <sikhululiwe.ngwenya@virtualhospitalsafrica.org>
2026-03-17 05:38:35 +0200

    Update 24-fever.lisp

--- s_expression/system_priority_evaluations/apc-adult/24-fever.lisp
+++ s_expression/system_priority_evaluations/apc-adult/24-fever.lisp
@@ -17,20 +17,36 @@
   (and
     (clinical_finding (snomed_concept "Fever" "finding"))
     (or
-      (clinical_finding (snomed_concept "Stiff neck" "finding"))
-      (clinical_finding (snomed_concept "Drowsy" "finding"))
-      (clinical_finding (snomed_concept "Confusional state" "disorder"))
-      (clinical_finding (snomed_concept "Purpuric rash" "disorder"))
-      (clinical_finding (snomed_concept "Difficulty breathing" "finding"))
-      (clinical_finding (snomed_concept "Tenderness of right lower quadrant of abdomen" "finding"))
-      (clinical_finding (snomed_concept "Acute abdominal pain" "finding"))
-      (clinical_finding (snomed_concept "Backache" "finding") (qualifier (snomed_concept "Severe (severity modifier)" "qualifier value")))
-      (clinical_finding (snomed_concept "Jaundice" "finding"))
-      (clinical_finding (snomed_concept "Easy bruising" "finding"))
-      (clinical_finding (snomed_concept "Finding of tendency to bleed" "finding"))
-      (< (measurement (snomed_concept "Systolic blood pressure" "observable entity") mmHg) 90)
-      (< (measurement (snomed_concept "Diastolic blood pressure" "observable entity") mmHg) 60)
-      (> (measurement (snomed_concept "Respiratory rate" "observable entity") bpm) 30)
-    )
-  )
+     (clinical_finding (snomed_concept “Seizure” (finding))
+		OR
+		(clinical_finding (snomed_concept “Post-ictal state (finding))
+		)
+		(clinical_finding (snomed_concept “Decreased level of consciousness (finding))
+		)
+		(clinical_finding (snomed_concept "Stiff neck" "finding"))
+		OR
+    (clinical_finding (snomed_concept "Drowsy" "finding"))
+		OR
+    (clinical_finding (snomed_concept "Confusional state" "disorder"))
+		OR 
+    (clinical_finding (snomed_concept "Purpuric rash" "disorder"))
+)
+    (> (measurement (snomed_concept "Respiratory rate" "observable entity") bpm) 30)
+		OR
+		(clinical_finding (snomed_concept "Difficulty breathing" "finding"))
+)
+		(< (measurement (snomed_concept "Systolic blood pressure" "observable entity") mmHg) 90))
+    (< (measurement (snomed_concept "Diastolic blood pressure" "observable entity") mmHg) 60)
+    (clinical_finding (snomed_concept "Tenderness of right lower quadrant of abdomen" "finding"))
+)
+    (clinical_finding (snomed_concept "Acute abdominal pain" "finding")) Severe” (severity modifier) (qualifier value))
+		OR
+    (clinical_finding (snomed_concept "Backache" "finding") "Severe” (severity modifier)" "qualifier value"))
+)
+    (clinical_finding (snomed_concept "Jaundice" "finding"))
+)
+    (clinical_finding (snomed_concept "Easy bruising" "finding"))
+		OR
+    (clinical_finding (snomed_concept "Finding of tendency to bleed" "finding"))
+      )
 )
