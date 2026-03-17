3ee174e93c41257db6e9a410a2b49e1a098dd2b9
Sikhululiwe Ngwenya <sikhululiwe.ngwenya@virtualhospitalsafrica.org>
2026-03-17 05:14:37 +0200

    Update 22-bites-and-stings.lisp

--- s_expression/system_diagnosis_rules/apc-adult/22-bites-and-stings.lisp
+++ s_expression/system_diagnosis_rules/apc-adult/22-bites-and-stings.lisp
@@ -1,12 +1,14 @@
-;; Page 22 - Bites and Stings: snake bite poisoning
+;; Page 22 - Bites and Stings
 (system_diagnosis_rule
   (diagnosis
-    (snomed_concept "Poisoning caused by venomous snake" "disorder")
-    possible
+    (snomed_concept "Prurigo simplex" "disorder")
+    probable
   )
   adult
   (or
-    (clinical_finding (snomed_concept "Snake bite - wound" "disorder"))
-    (finding (snomed_concept "Exposure to (contextual qualifier)" "qualifier value") (snomed_concept "Snake venom" "substance"))
-  )
+		(clinical_finding (snomed_concept “Prurigo simplex” (disorder))
+		(clinical_finding (snomed_concept “Hypersensitivity finding” (finding) “Bite of insect” (event))
+		(clinical_finding (snomed_concept “Papule” (morphologic abnormality) “Red color” (qualifier value) “Into” (attribute) “Blister” (morphologic abnormality))
+		(clinical_finding (snomed_concept “Structure resulting from tissue repair process” (morphologic abnormality) “Hyperpigmentation” (morphologic abnormality))
+	)
 )
