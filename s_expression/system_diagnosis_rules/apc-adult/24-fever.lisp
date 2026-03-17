45044f2016083ba74649cc75e084841c5799465f
Sikhululiwe Ngwenya <sikhululiwe.ngwenya@virtualhospitalsafrica.org>
2026-03-17 06:56:07 +0200

    Complicated Malaria likely Hb <7, glucose<3, unable to take medication orally, symptoms worsen

--- s_expression/system_diagnosis_rules/apc-adult/24-fever.lisp
+++ s_expression/system_diagnosis_rules/apc-adult/24-fever.lisp
@@ -30,4 +30,23 @@
     (clinical_finding (snomed_concept "Fever" "finding"))
     (clinical_finding (snomed_concept "Tenderness of right lower quadrant of abdomen" "finding"))
   )
+Page 24 - Fever: Complicated malaria” (disorder)
+(system_diagnosis_rule
+  (diagnosis
+    (snomed_concept Complicated malaria” (disorder)
+    probable
+		(clinical_finding (snomed_concept “Complicated malaria” (disorder)(
+		(<clinical_finding (snomed_concept “Measurement of total hemoglobin concentration” (procedure) “Gram/deciliter” (qualifier value) 7))					
+
+		(<clinical_finding (snomed_concept “Finding of blood glucose level” (finding) “Millimole/liter” (qualifier value) 3))
+		(clinical_finding (snomed_concept “Unable to take medication” (finding) “Oral route” (qualifier value))
+(clinical_finding (snomed_concept “Patient's condition worsened” (finding))
+(referral
+		(clinical_finding (snomed_concept “Urgent referral” (procedure) “In” (attribute) “hour” (qualifier value) 24))
+		(>clinical_finding (snomed_concept “Current chronological age (observable entity) year (qualifier value) 65))
+		(clinical_finding (snomed_concept “Pregnancy” (finding))
+		(clinical_finding (snomed_concept “Human immunodeficiency virus infection” (disorder) “Known” (qualifier value))
+	(clinical_finding (snomed_concept “Diabetes mellitus” (disorder))
+(clinical_finding (snomed_concept “Malaria” (disorder)  “Drug not available due to being out of stock” (finding)) 
+
 )
