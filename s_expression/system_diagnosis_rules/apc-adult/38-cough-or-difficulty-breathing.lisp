a1536e38a75910fe4e3ea296d5f336f56e18efab
Sikhululiwe Ngwenya <sikhululiwe.ngwenya@virtualhospitalsafrica.org>
2026-03-18 06:27:14 +0200

    Update 38-cough-or-difficulty-breathing.lisp

--- s_expression/system_diagnosis_rules/apc-adult/38-cough-or-difficulty-breathing.lisp
+++ s_expression/system_diagnosis_rules/apc-adult/38-cough-or-difficulty-breathing.lisp
@@ -27,3 +27,19 @@
     (< (measurement (snomed_concept "Systolic blood pressure" "observable entity") mmHg) 90)
   )
 )
+;; Page 38 - Cough/Breathing: Pneumonia likely
+(system_diagnosis_rule
+  (diagnosis
+    (snomed_concept "Pneumonia" "disorder")
+    probable 
+  )
+  adult
+			(<duration (clinical_finding (snomed_concept “Cough” (finding) “week” (qualifier value) 2))
+			(clinical_finding (snomed_concept “Cough” (finding) “No recurrence of problem” (situation))
+			(clinical_finding (snomed_concept “Productive cough” (finding))
+			ANY
+			(>=measurement (snomed_concept “Respiratory rate” (observable entity) “Breaths/minute” (qualifier value) 20))
+			(>= measurement (clinical_finding (snomed_concept “Pulse, function (observable entity) Beats/minute (qualifier value) 100))
+			(clinical_finding (snomed_concept “Opacity of bilateral lungs on plain chest X-ray” (finding))
+			(clinical_finding (snomed_concept “Auscultation” (procedure) “Both lungs” (body structure) ‘Respiratory crackles” (finding))
+			(clinical_finding (snomed_concept “Auscultation” (procedure) “Both lungs” (body structure) “Bronchial breathing” (finding))
