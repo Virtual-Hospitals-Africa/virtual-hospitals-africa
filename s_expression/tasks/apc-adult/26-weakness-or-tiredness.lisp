271c7e23c1dc69580ff45fb4976c235212bf8a61
Sikhululiwe Ngwenya <sikhululiwe.ngwenya@virtualhospitalsafrica.org>
2026-04-08 23:07:33 +0200

    Update 26-weakness-or-tiredness.lisp

--- s_expression/tasks/apc-adult/26-weakness-or-tiredness.lisp
+++ s_expression/tasks/apc-adult/26-weakness-or-tiredness.lisp
@@ -4,9 +4,9 @@
   adult
   (clinical_finding (snomed_concept "Fatigue" "finding"))
   (check_for
-    (clinical_finding (snomed_concept "Weakness of face muscles" "finding") (qualifier (snomed_concept "Sudden onset" "qualifier value")))
-    (clinical_finding (snomed_concept "Muscle weakness of upper limb" "finding") (qualifier (snomed_concept "Sudden onset" "qualifier value")))
-    (clinical_finding (snomed_concept "Weakness of muscle of lower limb" "finding") (qualifier (snomed_concept "Sudden onset" "qualifier value")))
+    (clinical_finding (snomed_concept "Weakness of face muscles" "finding") (qualifier (snomed_concept "Sudden" "qualifier value") "Asymmetry" (qualifier value))
+    (clinical_finding (snomed_concept "Muscle weakness of upper limb" "finding") (qualifier (snomed_concept "Sudden" "qualifier value") "Asymmetry" (qualifier value))
+    (clinical_finding (snomed_concept "Weakness of muscle of lower limb" "finding") (qualifier (snomed_concept "Sudden" "qualifier value") "Asymmetry" (qualifier value))
     (clinical_finding (snomed_concept "Numbness of limbs" "finding"))
     (clinical_finding (snomed_concept "Numbness of face" "finding"))
     (clinical_finding (snomed_concept "Difficulty talking" "finding"))
@@ -14,7 +14,7 @@
     (clinical_finding (snomed_concept "Chest pain" "finding"))
     (clinical_finding (snomed_concept "Difficulty breathing" "finding"))
     (clinical_finding (snomed_concept "Orthopnea" "finding"))
-    (clinical_finding (snomed_concept "Leg swelling symptom" "finding"))
+    (clinical_finding (snomed_concept “Swelling of lower limb” (finding))
     (clinical_finding (snomed_concept "Signs of dehydration" "finding"))
     (clinical_finding (snomed_concept "Thirst due to water deprivation" "finding"))
     (clinical_finding (snomed_concept "Xerostomia due to dehydration" "disorder"))
@@ -25,3 +25,4 @@
     (clinical_finding (snomed_concept "Anemia" "disorder"))
   )
 )
+
