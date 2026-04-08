facdd721e6c510b2c49f4e4e0533b8b1e91f65dd
Sikhululiwe Ngwenya <sikhululiwe.ngwenya@virtualhospitalsafrica.org>
2026-04-08 22:39:55 +0200

    Update 25-lump-swelling-in-neck-axilla-or-groin.lisp

--- s_expression/system_priority_evaluations/apc-adult/25-lump-swelling-in-neck-axilla-or-groin.lisp
+++ s_expression/system_priority_evaluations/apc-adult/25-lump-swelling-in-neck-axilla-or-groin.lisp
@@ -6,12 +6,31 @@
   (and
     (clinical_finding (snomed_concept "Groin mass" "finding"))
     (or
-      (clinical_finding (snomed_concept "Strangulated inguinal hernia" "disorder"))
-      (clinical_finding (snomed_concept "Irreducible inguinal hernia" "disorder"))
-      (clinical_finding (snomed_concept "Aneurysm" "disorder") (qualifier (snomed_concept "Pulsatile" "qualifier value")))
+      (clinical_finding (snomed_concept “Groin mass” (finding))
+			(clinical_finding (snomed_concept “Standing up” (observable entity) “Increased size” (finding))
+			(clinical_finding (snomed_concept “Coughing” (observable entity) “Increased size” (finding))
+			(clinical_finding (snomed_concept “Defecation” (observable entity) “Increased size” (finding))
+			ANY
       (clinical_finding (snomed_concept "Severe pain" "finding"))
-      (clinical_finding (snomed_concept "Finding of vomiting" "finding"))
-      (clinical_finding (snomed_concept "Unable to break wind" "finding"))
-    )
-  )
+      (clinical_finding (snomed_concept “Vomiting” (disorder))
+			(clinical_finding (snomed_concept “Constipation” (finding) “hour” (qualifier value) 24))
+      (clinical_finding (snomed_concept "Unable to break wind" "finding") “hour” (qualifier value) 24))
+			(clinical_finding (snomed_concept “Irreducible hernia” (morphologic abnormality))
+			(clinical_finding (snomed_concept “Groin mass” (finding)) Pulsatile” (qualifier value))
+		)
+	)
+)
+;; Page 25 - Lump/Swelling in Neck, Axilla or Groin
+(system_priority_evaluation
+  "Urgent: neck or axilla mass with obstruction or vascular signs"
+  adult
+  Urgent
+  (and
+    (clinical_finding (snomed_concept “Mass of neck” (finding))
+		(clinical_finding (snomed_concept “Mass of axilla” (finding)
+    (or
+			(clinical_finding (snomed_concept “Mass of neck” (finding) Pulsatile” (qualifier value))
+			OR
+			(clinical_finding (snomed_concept “Mass of axilla” (finding) Pulsatile” (qualifier value))
+	)
 )
