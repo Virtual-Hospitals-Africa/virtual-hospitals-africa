e6e1a6a5398623c8f0ea04aa07b8628b2db0ee23
Sikhululiwe Ngwenya <sikhululiwe.ngwenya@virtualhospitalsafrica.org>
2026-04-08 18:23:39 +0200

    Update 21-burns.lisp

--- s_expression/tasks/apc-adult/21-burns.lisp
+++ s_expression/tasks/apc-adult/21-burns.lisp
@@ -8,30 +8,42 @@
     (clinical_finding (snomed_concept "Clouded consciousness" "finding"))
     (clinical_finding (snomed_concept "Electrical burn" "disorder"))
     (clinical_finding (snomed_concept "Chemical burn" "disorder"))
-    (clinical_finding (snomed_concept "Full thickness burn" "disorder"))
+    (clinical_finding (snomed_concept “White discoloration of skin” (finding))
+		(clinical_finding (snomed_concept “Gray skin” (finding))
+		(clinical_finding (snomed_concept “Painless” (qualifier value)) 
+		(clinical_finding (snomed_concept “Xeroderma” (disorder))
     (clinical_finding (snomed_concept "Burn injury with charring" "morphologic abnormality"))
-    (clinical_finding (snomed_concept "Partial thickness burn" "disorder") (qualifier (snomed_concept "Extensive" "qualifier value")))
-    (clinical_finding (snomed_concept "Smoke inhalation injury" "disorder"))
+    (clinical_finding (snomed_concept “Pink skin” (finding))
+		(clinical_finding (snomed_concept “Redness of skin over lesion” (finding))
+		(clinical_finding (snomed_concept “Pain” (finding))
+		(clinical_finding (snomed_concept “Skin-ache syndrome” (finding))
+		(clinical_finding (snomed_concept “Blister” (morphologic abnormality))
+		(clinical_finding (snomed_concept “Moist skin”(finding))
+		(clinical_finding (snomed_concept “Burn of neck” (disorder))
+		(clinical_finding (snomed_concept “Burn of face” (disorder))
     (clinical_finding (snomed_concept "Difficulty breathing" "finding"))
     (clinical_finding (snomed_concept "Hoarse" "finding"))
     (clinical_finding (snomed_concept "Stridor" "finding"))
-    (clinical_finding (snomed_concept "Dirty sputum" "finding"))
-    (clinical_finding (snomed_concept "Burn" "disorder") (finding_site (snomed_concept "Neck structure" "body structure")))
-    (clinical_finding (snomed_concept "Burn" "disorder") (finding_site (snomed_concept "Chest structure" "body structure")) (qualifier (snomed_concept "Circumferential" "qualifier value")))
-    (clinical_finding (snomed_concept "Burn" "disorder") (finding_site (snomed_concept "Limb structure" "body structure")) (qualifier (snomed_concept "Circumferential" "qualifier value")))
-    (clinical_finding (snomed_concept "Burn" "disorder") (finding_site (snomed_concept "Face structure" "body structure")))
-    (clinical_finding (snomed_concept "Burn" "disorder") (finding_site (snomed_concept "Hand structure" "body structure")))
-    (clinical_finding (snomed_concept "Burn" "disorder") (finding_site (snomed_concept "Foot structure" "body structure")))
-    (clinical_finding (snomed_concept "Burn" "disorder") (finding_site (snomed_concept "Genital structure" "body structure")))
-    (clinical_finding (snomed_concept "Burn" "disorder") (finding_site (snomed_concept "Joint structure" "body structure")))
+    (clinical_finding (snomed_concept “Finding of color of sputum” (finding) “Black color” (qualifier value))
+		(clinical_finding (snomed_concept “Brown sputum” (finding))
+		(clinical_finding (snomed_concept “Gray sputum” (finding)) 
+  	(clinical_finding (snomed_concept “Burn of chest wall” (disorder) "Circumferential" "qualifier value")))
+    (clinical_finding (snomed_concept “Burn of lower limb” (disorder) “Circumferential” (qualifier value))
+		(clinical_finding (snomed_concept “Burn of upper limb (disorder) “Circumferential” (qualifier value))
+    (clinical_finding (snomed_concept “Burn of hand” (disorder))
+    (clinical_finding (snomed_concept “Burn of foot” (disorder))
+    (clinical_finding (snomed_concept “Burn of genitalia” (disorder))
+    (clinical_finding (snomed_concept “Burn” (disorder) “Joint structure of limb” (body structure))
+		(<measurement (clinical_finding (snomed_concept “Hemoglobin saturation with oxygen” (observable entity) “Percent” (property) (qualifier value) 94))
     (clinical_finding (snomed_concept "Traumatic injury" "disorder"))
+  	)
   )
 )
-(task
-  "Measure Bloody Oxygen Saturation due to burns"
-  adult
-  (clinical_finding (snomed_concept "Burn" "disorder"))
-  (measure
-    (measurement (snomed_concept "Hemoglobin saturation with oxygen" "observable entity") %)
-  )
-)
+
+
+
+
+
+
+
+
